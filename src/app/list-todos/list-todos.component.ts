83d5c3a71e9b0d2aa76a32535df463793a30cbc2
nkdey14 <im.pandey264@gmail.com>
2026-05-02 21:26:01 +0530

    Updated ListTodos component to add TargetDate and IsCompleted columns

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date,
+  ) {}
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,9 +16,14 @@ import { Component } from '@angular/core';
 })
 export class ListTodosComponent {
   todos = [
-    { id: 1, description: 'Learn to Dance' },
-    { id: 2, description: 'Become an Expert in Angular' },
-    { id: 3, description: 'Visit India' },
-    { id: 4, description: 'Buy a Car' },
+    new Todo(1, 'Learn to Dance', false, new Date()),
+    new Todo(2, 'Become an Expert in Angular', false, new Date()),
+    new Todo(3, 'Visit India', false, new Date()),
+    new Todo(4, 'Buy a Car', false, new Date()),
+
+    // { id: 1, description: 'Learn to Dance' },
+    // { id: 2, description: 'Become an Expert in Angular' },
+    // { id: 3, description: 'Visit India' },
+    // { id: 4, description: 'Buy a Car' },
   ];
 }
